9c971f22fceb5d40a42c05af3537cb620be2bb14
Maciek Sitkowski <msitkowski94@gmail.com>
2025-01-21 15:03:40 +0100

    add prettier

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,4 +1,4 @@
-import {GlobeAltIcon} from '@heroicons/react/24/outline'
+import { GlobeAltIcon } from '@heroicons/react/24/outline'
 // import { lusitana } from '#/app/ui/fonts';
 
 export default function AcmeLogo() {
