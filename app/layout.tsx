8c7813c71f8734170513ec32442cc5442385b1fa
Maciek Sitkowski <msitkowski94@gmail.com>
2025-01-24 19:43:48 +0100

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,16 @@
 import '@/app/globals.css'
 import { inter } from '@/app/ui/fonts'
 import { PropsWithChildren } from 'react'
+import { Metadata } from 'next'
 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+}
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
